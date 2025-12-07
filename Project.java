72d2af47bc848a1441f922d702ec3ef50472f702
Mohammad Rimawi <mohammadrimawi47@gmail.com>
2025-12-07 20:42:50 +0200

    test 2

--- Project.java
+++ Project.java
@@ -1,5 +1,12 @@
 public class Project {
     public static void main(String[] args) {
+        int[] array = { 4, 1, 6, 9, 2, 3, 7, 8, 5 };
         System.out.println("Hello World");
+        insertionSort(array);
+
+    }
+
+    static void insertionSort(int[] array) {
+
     }
 }
